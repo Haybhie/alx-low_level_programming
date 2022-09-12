ea4a31771fc969abfc56d645e22427e86f23d59d
Haybhiee <Haybhie@example.com>
2022-09-12 16:53:25 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+
+
+
 /**
- *    * Description: main - Prints out a string to stdout.
- *        * Return: 0 if success.
- *             */
+ *
+ *  * main - prints the phase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+
 }
